948f0865391166d9e2b1d48c0661b45fa68f8eb7
MAtterholt <matterholt.dev@gmail.com>
2025-04-30 23:52:58 -0400

    geting site built

--- site/src/components/base/index.js
+++ site/src/components/base/index.js
@@ -1,4 +1,6 @@
 import Text from "./Text.astro";
 import Link from "./Link.astro";
+import Box from "./Box.astro";
+import List from "./List.astro";
 
-export { Link, Text };
+export { Link, Text, Box, List };
