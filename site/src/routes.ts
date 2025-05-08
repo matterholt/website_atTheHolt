b00d4428755a21179a1b49af41629bc97255036c
MAtterholt <matterholt.dev@gmail.com>
2025-05-07 23:20:49 -0400

    prodressing foward

---
+++ site/src/routes.ts
@@ -0,0 +1,68 @@
+export const toolsLinks = [
+  {
+    name: "Pearson Square",
+    url: "/tools/pearson-square",
+    isActive: true,
+    linkType: "internal",
+  },
+  {
+    name: "Gestations Cycles",
+    url: "/tools/gestations-cycles",
+    isActive: true,
+    linkType: "internal",
+  },
+];
+export const contactLinks = [
+  {
+    name: "Email",
+    url: "mailto:matthew@attheholt",
+    isActive: true,
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Facebook",
+    linkType: "external",
+    isActive: true,
+    url: "https://www.facebook.com/profile.php?id=61570295491797",
+  },
+];
+
+export const otherResources = [
+  {
+    name: "Working Cow Podcast",
+    linkType: "external",
+    isActive: true,
+    url: "https://workingcows.net/",
+  },
+];
+
+export const internalLinks = [
+  {
+    name: "Home",
+    url: "/",
+    isActive: true,
+  },
+  {
+    name: "Online Tools",
+    url: "/online-tools",
+    isActive: true,
+  },
+  {
+    name: "Future Plans",
+    url: "/future-plans",
+    isActive: true,
+  },
+  {
+    name: "Our Animals",
+    url: "/animals",
+    isActive: true,
+  },
+
+  {
+    name: "Projects",
+    url: "/projects",
+    isActive: true,
+  },
+];
