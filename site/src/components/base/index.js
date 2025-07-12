5f21c8b199135c9df2d6a8c60e032cf8bc3d056a
MAtterholt <matterholt.dev@gmail.com>
2025-07-12 10:22:06 -0400

    todays work

--- site/src/components/base/index.js
+++ site/src/components/base/index.js
@@ -1,6 +1,6 @@
 import Text from "./Text.astro";
 import Link from "./Link.astro";
 import Box from "./Box.astro";
-import List from "./List.astro";
+import LinkList from "./LinkList.astro";
 
-export { Link, Text, Box, List };
+export { Link, Text, Box, LinkList };
